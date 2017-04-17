4ffd02f097beb8891851f49a07d8f143f8bce247
shadow3x3x3 <shadow3x3x3@gmail.com>
2017-04-17 09:26:20 +0800

    add spec

--- spec/skyline-cr_spec.cr
+++ spec/skyline-cr_spec.cr
@@ -1,9 +1,26 @@
 require "./spec_helper"
 
 describe Skyline::Cr do
-  # TODO: Write tests
+  context "#dominate?" do
+    it "Dominate" do
+      result = Skyline::Cr.dominate? [1, 2, 3], [2, 3, 4]
+      result.should eq(Skyline::Cr::Domination::Dominate)
+    end
 
-  it "works" do
-    false.should eq(true)
+    it "Non Dominate" do
+      result = Skyline::Cr.dominate? [1, 2, 3], [2, 3, 1]
+      result.should eq(Skyline::Cr::Domination::NonDominate)
+    end
+
+    it "Be Dominated" do
+      result = Skyline::Cr.dominate? [2, 3, 4], [1, 2, 3]
+      result.should eq(Skyline::Cr::Domination::BeDominated)
+    end
+
+    it "Array size not match" do
+      expect_raises(SkylineException, "Two Attributes size is not match.") do
+        Skyline::Cr.dominate? [2, 3, 4], [1, 2, 3, 1]
+      end
+    end
   end
 end
